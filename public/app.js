066fdbd5a4e95332ef040288eed529b304a409ae
Andrii Litvinov <andriilitvinov@air-andrii.lan>
2026-04-18 09:19:46 -0400

    Restore header projects button

--- public/app.js
+++ public/app.js
@@ -26,7 +26,7 @@ const state = {
   visibleCommandUpdates: {}
 };
 
-const BUILD_VERSION = "20260418-0649";
+const BUILD_VERSION = "20260418-0656";
 const SPEED_POLL_INTERVAL_MS = 1000;
 const SPEED_POLL_WINDOW_MS = 25000;
 const FAST_POLL_INTERVAL_MS = 3500;
