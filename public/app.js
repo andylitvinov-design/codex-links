58d2e65b4005cff53badf8394508f852b3606eb8
Andrii Litvinov <andriilitvinov@air-andrii.lan>
2026-04-22 12:37:12 -0400

    Harden photo delivery routes

--- public/app.js
+++ public/app.js
@@ -36,7 +36,11 @@ const state = {
   visibleCommandUpdates: {}
 };
 
+<<<<<<< HEAD
 const BUILD_VERSION = "20260423-1225";
+=======
+const BUILD_VERSION = "20260422-1645";
+>>>>>>> 739883e (Harden photo delivery routes)
 const SPEED_POLL_INTERVAL_MS = 1000;
 const SPEED_POLL_WINDOW_MS = 25000;
 const FAST_POLL_INTERVAL_MS = 3500;
